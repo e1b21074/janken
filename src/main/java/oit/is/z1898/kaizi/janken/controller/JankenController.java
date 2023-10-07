f55c6c3dba639539e5b33ab858f810d4d544db24
e1b21074 <e1b21074@oit.ac.jp>
2023-10-07 17:13:17 +0900

    work02の実装完了

--- src/main/java/oit/is/z1898/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z1898/kaizi/janken/controller/JankenController.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +22,27 @@ public class JankenController {
   @PostMapping("/janken")
   public String janken(@RequestParam String name, ModelMap model) {
     model.addAttribute("name", name);
+    return "janken.html";
+  }
+
+  @GetMapping("/jankengame")
+  public String gu(@RequestParam String Playerhand, ModelMap model) {
+    String Result = "";
+    String Cpuhand = "gu";
+    switch (Playerhand) {
+      case "gu":
+        Result = "Draw";
+        break;
+      case "choki":
+        Result = "You Lose";
+        break;
+      case "pa":
+        Result = "You Win";
+        break;
+    }
+    model.addAttribute("Playerhand", Playerhand);
+    model.addAttribute("Cpuhand", Cpuhand);
+    model.addAttribute("Result", Result);
     return "janken.html";
   }
 
